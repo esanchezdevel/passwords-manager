f2c293eb37ac761709a97d54423d3d4216a3c252
enrique.sanchez <enrique.sanchez@hpe.com>
2025-04-09 16:40:59 +0200

    refactor: remove unused imports

--- src/main/java/com/passwords/manager/controller/WelcomeViewController.java
+++ src/main/java/com/passwords/manager/controller/WelcomeViewController.java
@@ -1,13 +1,10 @@
 package com.passwords.manager.controller;
 
-import java.util.Optional;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import com.passwords.manager.core.cdi.App;
 import com.passwords.manager.core.cdi.annotation.Inject;
-import com.passwords.manager.domain.model.Credential;
 import com.passwords.manager.domain.service.CredentialService;
 
 import javafx.fxml.FXML;
