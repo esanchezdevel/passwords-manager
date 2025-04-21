ff5c61ae34f1b844e214aa39a661e0488364a496
enrique.sanchez <enrique.sanchez@hpe.com>
2025-04-21 13:23:05 +0200

    feat: add icon to view title

--- src/main/java/com/passwords/manager/controller/WelcomeViewController.java
+++ src/main/java/com/passwords/manager/controller/WelcomeViewController.java
@@ -14,7 +14,6 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
